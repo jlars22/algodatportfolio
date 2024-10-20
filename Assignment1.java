5c0bc67dc8ddd9e530429ab4e854c01b81514e25
Jonas Larsen <joonaslarsen@gmail.com>
2024-10-20 11:04:14 +0200

    initial commit

---
+++ Assignment1.java
@@ -0,0 +1,13 @@
+class Assignment1 {
+    public static void main(String[] args) {
+        System.out.println(sumOddSquares(8));
+    }
+
+    private static int sumOddSquares(int n) {
+        if (n <= 0) return 0;
+
+        if (n % 2 == 0) n--;
+
+        return n * n + sumOddSquares(n - 1);
+    }
+}
